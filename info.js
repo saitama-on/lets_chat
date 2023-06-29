f24f2e843ab2e0f0f4c13d5a56aa903a65f14984
saitama-on <yashjoshi6787@gmail.com>
2023-06-29 17:21:31 +0530

    nothing much

---
+++ info.js
@@ -0,0 +1,12 @@
+
+   const apiKey = "AIzaSyBIbRFofcWrkdjbq0y66RCFE3WZyCDrdfE";
+   const authDomain="chat-23fe4.firebaseapp.com";
+   const projectId="chat-23fe4";
+   const storageBucket="chat-23fe4.appspot.com";
+   const messagingSenderId="493468911945";
+   const appId="1:493468911945:web:077e9067658640a9fcf298";
+   const measurementId="G-9NH4NRGGP1";
+   const databaseURL="https://chat-23fe4-default-rtdb.firebaseio.com";
+ 
+
+   export{ apiKey, authDomain, projectId, messagingSenderId , storageBucket, appId, measurementId , databaseURL };
